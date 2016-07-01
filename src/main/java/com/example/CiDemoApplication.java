adf02b88d95635877921a8d6eca415f743b8de2b
Kan Nakamura <knakamura@pivotal.io>
2016-07-01 15:57:48 +0900

    start from v0

--- src/main/java/com/example/CiDemoApplication.java
+++ src/main/java/com/example/CiDemoApplication.java
@@ -11,7 +11,7 @@ public class CiDemoApplication {
 
     @RequestMapping("/")
     String hello() {
-        return "Hello World V1!";
+        return "Hello World V0!";
     }
 
     public static void main(String[] args) {
--- src/test/java/com/example/CiDemoApplicationTests.java
+++ src/test/java/com/example/CiDemoApplicationTests.java
@@ -22,7 +22,7 @@ public class CiDemoApplicationTests {
 
     @Test
     public void contextLoads() {
-        assertThat(restTemplate.getForObject("http://localhost:" + port, String.class), is("Hello World V1!"));
+        assertThat(restTemplate.getForObject("http://localhost:" + port, String.class), is("Hello World V0!"));
     }
 
 }
